ce1c866d916a9c69780810f11f98b9ec8b4cc96c
5murdok5 <santiago.moreno52@gmail.com>
2023-12-05 18:38:53 -0500

    correccion configuracion de github

--- proyect/src/App.jsx
+++ proyect/src/App.jsx
@@ -1,98 +1,140 @@
-import { useEffect, useState } from 'react';
-//import viteLogo from '../public/vite.svg'
-import {db} from './data/configFirebase'
-import './App.css'
+import { useEffect, useState } from "react";
+import { db } from "./data/config_firebase";
 
-
-/* const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Get a list of cities from your database
-async function getTasks() {
-  const taskCol = collection(db, 'tasks');
-  const taskSnapshot = await getDocs(taskCol);
-  const taskList = taskSnapshot.docs.map(doc => doc.data());
-  return taskList;
-} */
-
-/* async function postTasks() {
-  const tasks = collection(db, 'tasks');
-  await addDoc(tasks,{"nombre":"miguel","apellido":"parra"});
-} */
-
-
-
-async function App() {
-  //const [count, setCount] = useState(0);
-  /* const Task = {
-    nombre: '',
-    apellido: false,
-    id:null,  
-  }; */
-  //const [ tasks, setTasks ] = useState<Task>([])
-
-const initvl={tasks:[]}
-
-
- const [data1, setData1]=useState(initvl)
- /*  setTasks(firebaseApp
-    .firestore()
-    .collection('tasks')); */
-  //const tasks = useFirestoreCollection(tasksRef).docs.map(d => ({id: d.id, ...d.data()})); */
-  
-  
-
-  /* const taskCol = collection(db, 'tasks');
-  const taskSnapshot = await getDocs(taskCol);
-
-  
-  const tasks = taskSnapshot.docs.map(doc => doc.data());
-  console.log(tasks)
- */
+const App = () => {
+  const initVls = {
+    tasks: [],
+  };
+  const [values, setValues] = useState(initVls);
 
   useEffect(() => {
-  
-    getData();
-    
-  },[])
+    const action = async () => getData();
+    action();
+  }, []);
 
-  const getData =async()=>{
+  const getData = async () => {
     try {
-      const data=await db.collection('tasks').get().then(data=>{
-        console.log(data);
-        return data.docs.map(doc => doc.data());
-      })
-      
-      setData1({...data1,tasks:data})
+      const resp = await db
+        .collection("tasks")
+        .get()
+        .then((data) => {
+          return data.docs.map((vl) => vl.data());
+        })
+        .catch((e) => {
+          console.log(e);
+          return [];
+        });
+      setValues({ ...values, tasks: resp });
     } catch (error) {
       console.log(error);
+      setValues({ ...values, tasks: [] });
+      return [];
     }
   };
-  /* setTasks([
-    {"id":'1',"nombre":"carlos","apellido":"torres"},
-    {"id":'2',"nombre":"cesar","apellido":"villa"}
-  ]) */
-
-  //tasks.push({"id":'1',"nombre":"carlos","apellido":"torres"});
-  //tasks.push({"id":'2',"nombre":"cesar","apellido":"villa"});
   return (
-    <>
-
-      <h1>My favorite color is {color}!</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+    <div>
+      <h1>Tareas</h1>
       <ul>
-        {(data1.tasks??[]).map((task) => (
-          <li key={task.nombre}>{task.nombre}.........{task.apellido}</li>
+        {values?.tasks.map((task, index) => (
+          <li key={index}>{task.nombre}</li>
         ))}
       </ul>
-      
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+// import { useEffect, useState } from "react";
+// import "./App.css";
+// import { db } from "./data/config_firebase";
+
+// /* const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// // Get a list of cities from your database
+// async function getTasks() {
+//   const taskCol = collection(db, 'tasks');
+//   const taskSnapshot = await getDocs(taskCol);
+//   const taskList = taskSnapshot.docs.map(doc => doc.data());
+//   return taskList;
+// } */
+
+// /* async function postTasks() {
+//   const tasks = collection(db, 'tasks');
+//   await addDoc(tasks,{"nombre":"miguel","apellido":"parra"});
+// } */
+// /*  setTasks(firebaseApp
+//     .firestore()
+//     .collection('tasks')); */
+// //const tasks = useFirestoreCollection(tasksRef).docs.map(d => ({id: d.id, ...d.data()})); */
+
+// /* const taskCol = collection(db, 'tasks');
+//   const taskSnapshot = await getDocs(taskCol);
+
+//   const tasks = taskSnapshot.docs.map(doc => doc.data());
+//   console.log(tasks)
+//  */
+// //const [count, setCount] = useState(0);
+// const Task = {
+//   nombre: '',
+//   apellido: false,
+//   id:null,
+// };
+// const [ tasks, setTasks ] = useState<Task>([])
+// async function App() {
+//   // const initvl = { tasks: [] };
+
+//   // const [values, setValues] = useState(initvl);
+
+// useEffect(() => {
+//   const action = async () => getData();
+//   action();
+// }, []);
+
+// const getData = async () => {
+//   try {
+//     const resp = await db
+//       .collection("tasks")
+//       .get()
+//       .then((data) => {
+//         console.log("data firebase:", data);
+//         return data.docs.map((vl) => vl.data());
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//         return [];
+//       });
+//     console.log("resp fb: ", resp);
+//     setValues({ ...values, tasks: resp });
+//   } catch (error) {
+//     console.log(error);
+//     setValues({ ...values, tasks: [] });
+//     return [];
+//   }
+// };
+
+//   /* setTasks([
+//     {"id":'1',"nombre":"carlos","apellido":"torres"},
+//     {"id":'2',"nombre":"cesar","apellido":"villa"}
+//   ]) */
+
+//   //tasks.push({"id":'1',"nombre":"carlos","apellido":"torres"});
+//   //tasks.push({"id":'2',"nombre":"cesar","apellido":"villa"});
+//   return (
+//     <div>Hellow</div>
+//     // <>
+//     //   {/* <h1>My favorite color is {color}!</h1> */}
+
+//     //   <ul>
+//     //     {/* {(values.tasks ?? []).map((task) => (
+//     //       <li key={task.nombre}>
+//     //         {task.nombre}.........{task.apellido}
+//     //       </li>
+//     //     ))} */}
+//     //   </ul>
+//     // </>
+//   );
+// }
+
+// export default App;
--- proyect/src/data/config_firebase.jsx
+++ proyect/src/data/config_firebase.jsx
@@ -6,18 +6,17 @@ import { getStorage } from "firebase/storage";
 
 // import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-    apiKey: "AIzaSyBeocea8foBTEwkezrgudIcGMQ7NnHgs2E",
-    authDomain: "rapicourier-fd256.firebaseapp.com",
-    projectId: "rapicourier-fd256",
-    storageBucket: "rapicourier-fd256.appspot.com",
-    messagingSenderId: "284289082978",
-    appId: "1:284289082978:web:e0e66a356ad8d2920a533b",
-    measurementId: "G-4316T6CJL4"
+  apiKey: "AIzaSyBeocea8foBTEwkezrgudIcGMQ7NnHgs2E",
+  authDomain: "rapicourier-fd256.firebaseapp.com",
+  projectId: "rapicourier-fd256",
+  storageBucket: "rapicourier-fd256.appspot.com",
+  messagingSenderId: "284289082978",
+  appId: "1:284289082978:web:e0e66a356ad8d2920a533b",
+  measurementId: "G-4316T6CJL4",
 };
 
-
 const getConection = () => {
-    return firebase.initializeApp(firebaseConfig);
+  return firebase.initializeApp(firebaseConfig);
 };
 // Initialize Firebase
 var fb = getConection();
